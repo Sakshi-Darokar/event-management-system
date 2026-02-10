63f4e483e99e08d8025b713aae2fbc2fe40a2089
Sakshi Darokar <164064604+Sakshi-Darokar@users.noreply.github.com>
2026-02-10 18:15:19 +0530

    Clean up App component by removing commented code
    Removed commented-out code and unused routes for UserDashboard and AdminDashboard.

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -1,30 +1,8 @@
-// import React from "react";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Home from "./home";
-// import Login from "./auth/Login";
-// import Register from "./auth/Register";
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/login" element={<Login />} />
-//         <Route path="/register" element={<Register />} />
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
-
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./home";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
-import UserDashboard from "./user/UserDashboard";
-import AdminDashboard from "./admin/AdminDashboard";
 
 function App() {
   return (
@@ -33,12 +11,10 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-
-        <Route path="/user-dashboard" element={<UserDashboard />} />
-        <Route path="/admin-dashboard" element={<AdminDashboard />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
+
